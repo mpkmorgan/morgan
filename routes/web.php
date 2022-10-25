9d524d4f54554e1d0d8df7dae2232545b00dce27
mpkmorgan <39268404+mpkmorgan@users.noreply.github.com>
2022-10-26 01:18:31 +0300

    MAil fix
    err

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home',[HomeController::class,'redirect'])->
-middleware('auth','verified');
+Route::get('/home',[HomeController::class,'redirect']);
+            
 
 Route::middleware([
     'auth:sanctum',
--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -43,6 +43,8 @@ class RegistrationTest extends TestCase
         $response = $this->post('/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'phone' => '11111111',
+            'address' => 'address',
             'password' => 'password',
             'password_confirmation' => 'password',
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
