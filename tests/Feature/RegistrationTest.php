5ccbc5e545318ee8717c7152cb64e32e2d233e87
mpkmorgan <39268404+mpkmorgan@users.noreply.github.com>
2022-10-26 01:18:57 +0300

    Revert "MAil fix"
    This reverts commit 9d524d4f54554e1d0d8df7dae2232545b00dce27.

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -43,8 +43,6 @@ class RegistrationTest extends TestCase
         $response = $this->post('/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
-            'phone' => '11111111',
-            'address' => 'address',
             'password' => 'password',
             'password_confirmation' => 'password',
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
