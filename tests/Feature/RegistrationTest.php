760bad2e55a13b8f88d9f28e0806c4c97fa09845
mpkmorgan <39268404+mpkmorgan@users.noreply.github.com>
2022-10-31 15:22:23 +0300

    Tests

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -43,7 +43,7 @@ class RegistrationTest extends TestCase
         $response = $this->post('/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
-            'password' => 'passwor',
+            'password' => 'password',
             'password_confirmation' => 'password',
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
         ]);
